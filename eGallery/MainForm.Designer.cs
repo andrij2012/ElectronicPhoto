801500949659feadcab14863e5bbf6e7f92d43b0
Andriy Valkiv <andrij2012@gmail.com>
2014-11-22 19:57:34 +0200

    add simple event handlers (next, prev, load image, scan folder, play)\n TODO: implement classes and pretty design

---
+++ eGallery/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace eGallery
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.prevButton = new System.Windows.Forms.Button();
+            this.nexButton = new System.Windows.Forms.Button();
+            this.galleryPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryPictureBox)).BeginInit();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // prevButton
+            // 
+            this.prevButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.prevButton.BackColor = System.Drawing.Color.Transparent;
+            this.prevButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.prevButton.FlatAppearance.BorderSize = 0;
+            this.prevButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.prevButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.prevButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prevButton.ForeColor = System.Drawing.Color.Transparent;
+            this.prevButton.Image = ((System.Drawing.Image)(resources.GetObject("prevButton.Image")));
+            this.prevButton.Location = new System.Drawing.Point(12, 391);
+            this.prevButton.MaximumSize = new System.Drawing.Size(0, 40);
+            this.prevButton.MinimumSize = new System.Drawing.Size(40, 40);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(40, 40);
+            this.prevButton.TabIndex = 0;
+            this.prevButton.UseVisualStyleBackColor = false;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // nexButton
+            // 
+            this.nexButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.nexButton.BackColor = System.Drawing.Color.Transparent;
+            this.nexButton.FlatAppearance.BorderSize = 0;
+            this.nexButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.nexButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.nexButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nexButton.ForeColor = System.Drawing.Color.Transparent;
+            this.nexButton.Image = ((System.Drawing.Image)(resources.GetObject("nexButton.Image")));
+            this.nexButton.Location = new System.Drawing.Point(729, 391);
+            this.nexButton.MaximumSize = new System.Drawing.Size(40, 40);
+            this.nexButton.MinimumSize = new System.Drawing.Size(40, 40);
+            this.nexButton.Name = "nexButton";
+            this.nexButton.Size = new System.Drawing.Size(40, 40);
+            this.nexButton.TabIndex = 1;
+            this.nexButton.UseVisualStyleBackColor = false;
+            this.nexButton.Click += new System.EventHandler(this.nexButton_Click);
+            // 
+            // galleryPictureBox
+            // 
+            this.galleryPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.galleryPictureBox.Location = new System.Drawing.Point(12, 27);
+            this.galleryPictureBox.Name = "galleryPictureBox";
+            this.galleryPictureBox.Size = new System.Drawing.Size(757, 358);
+            this.galleryPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.galleryPictureBox.TabIndex = 2;
+            this.galleryPictureBox.TabStop = false;
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.BackColor = System.Drawing.Color.Transparent;
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(786, 24);
+            this.mainMenuStrip.TabIndex = 3;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open folder";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatAppearance.BorderSize = 0;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.ForeColor = System.Drawing.Color.Transparent;
+            this.playButton.Image = ((System.Drawing.Image)(resources.GetObject("playButton.Image")));
+            this.playButton.Location = new System.Drawing.Point(355, 392);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 39);
+            this.playButton.TabIndex = 4;
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(786, 581);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.galleryPictureBox);
+            this.Controls.Add(this.nexButton);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.mainMenuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Electronic Gallery";
+            ((System.ComponentModel.ISupportInitialize)(this.galleryPictureBox)).EndInit();
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button nexButton;
+        private System.Windows.Forms.PictureBox galleryPictureBox;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
